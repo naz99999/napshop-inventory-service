384247857e8f0e00f3d759654af2be182d872a1a
naman-bindra <bindra.n@turing.com>
2025-12-23 21:39:17 +0530

    added swagger

---
+++ src/main/java/com/napshop/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package com.napshop.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Inventory Service API")
+                        .version("v1"));
+    }
+}
